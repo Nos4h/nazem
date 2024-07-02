7f51d92db211df50d45a26cab32da7b35e84633d
Amlly7888 <amlly7888@gmail.com>
2024-07-02 11:41:31 +0300

    AdminDashboard.vue

---
+++ AdminDashboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="admin-dashboard">
+      <h1>Welcome to the Admin Dashboard</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AdminDashboard'
+  };
+  </script>
+  
+  <style scoped>
+  #admin-dashboard {
+    padding: 20px;
+    text-align: center;
+  }
+  </style>
